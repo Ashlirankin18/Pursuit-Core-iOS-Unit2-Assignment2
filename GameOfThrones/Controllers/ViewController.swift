1c0a76325c200bc92716391bff5988eb8616d359
Ashlirankin18 <ashlirankin18@gmail.com>
2018-11-19 18:42:55 -0500

    updated model to include matrix

--- GameOfThrones/Controllers/ViewController.swift
+++ GameOfThrones/Controllers/ViewController.swift
@@ -13,30 +13,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     
  private var episodes = GOTEpisode.allEpisodes
+    
     override func viewDidLoad() {
     super.viewDidLoad()
         episodeTableView.dataSource = self
         episodeTableView.delegate = self
-        searchBar.delegate = self
+       searchBar.delegate = self
   }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let indexPath = episodeTableView.indexPathForSelectedRow,
             let episodeDetails = segue .destination as? EpisodeDetails else {return}
-        let episode = episodes[indexPath.row]
+        let episode = episodes[indexPath.section][indexPath.row]
        episodeDetails.episode = episode
     }
 
 }
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
        return episodes.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return episodes[section].count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Season 1"
+        case 1:
+            return "Season 2"
+        case 2:
+            return "Season 3"
+        case 3:
+            return "Season 4"
+        case 4:
+            return "Season 5"
+        case 5:
+            return "Season 6"
+        case 6:
+            return "Season 7"
+        default:
+            return "invalid"
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let episode = episodes[indexPath.row]
+      let episode = episodes[indexPath.section][indexPath.row]
         switch episode.season {
         case 2,4,6:
-            let episode = episodes[indexPath.row]
+            let episode = episodes[indexPath.section][indexPath.row]
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "EpisodeOverviewCell", for: indexPath) as? episodeOverviewCell else { fatalError("episode not found")}
             cell.secondImageView.image = UIImage.init(named: episode.mediumImageID)
             cell.episodeName.text = episode.name
@@ -44,7 +68,7 @@ extension ViewController: UITableViewDataSource {
             return cell
         case 1,3,5,7:
          guard let cell2 = tableView.dequeueReusableCell(withIdentifier: "SecondOverviewCell", for: indexPath) as? secondOverviewCell else { fatalError("episode not found")}
-         let episode = episodes[indexPath.row]
+         let episode = episodes[indexPath.section][indexPath.row]
          cell2.overViewImage.image = UIImage.init(named: episode.mediumImageID)
          cell2.episodeName.text = episode.name
          cell2.episodeInfo.text = "S : \(episode.season) E: \(episode.number)"
@@ -66,8 +90,9 @@ extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         guard let searchText = searchBar.text else { return }
-        episodes = GOTEpisode.allEpisodes.filter
-            {$0.name.lowercased().contains(searchText.lowercased())}
-        
+        let searchResult = episodes.filter{$0[0].name == searchText}
+           
+        }
 }
-}
+    
+
